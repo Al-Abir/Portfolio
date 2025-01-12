e87f42096d7320771a0ad1f7c01ecbcafe68aaff
Al-Abir <sheikhabir196@gmail.com>
2025-01-12 22:48:30 +0600

    About

--- Portfolio-app/src/Components/About/About.jsx
+++ Portfolio-app/src/Components/About/About.jsx
@@ -13,20 +13,20 @@ const aboutItems = [
 
 const About = () => {
     return (
-        <section id="about" className="section">
+        <section id="about" className="section ml-4">
             <div className="container mx-auto bg-zinc-800/50 rounded-2xl md:p-12">
                 <div>
-                    <p>
+                    <p className=' text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]'>
                         Welcome! I&apos;m Henry, a professional web developer with a knack for crafting visually stunning
                         and highly functional websites. Combining creativity and technical expertise, I transform your vision into a digital masterpiece that excels in both appearance and performance.
                     </p>
-                    <div>
+                    <div className='flex flex-wrap items-center gap-4 md:gap-7'>
                         {
                             aboutItems.map(({ label, number }, key) => (
                                 <div key={key} className="about-item">
-                                    <div className="number-container">
-                                        <span className="number">{number}</span>
-                                        <span className="plus">+</span>
+                                    <div className="flex itmes-center md:mb-2">
+                                        <span className="text-2xl font-bold md:text-4xl">{number}</span>
+                                        <span className="text-sky-400 font-semibold text-2xl md:text-3xl">+</span>
                                     </div>
                                     <p className="label">
                                         {label}
